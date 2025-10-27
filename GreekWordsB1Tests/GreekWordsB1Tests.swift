2a5711d90cca8ffbbbf77996431bf4d02af5b147
Azonaz <malinina.a@gmail.com>
2025-10-27 12:12:08 +0200

    Initial Commit

---
+++ GreekWordsB1Tests/GreekWordsB1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GreekWordsB1Tests.swift
+//  GreekWordsB1Tests
+//
+//  Created by Админ on 27/10/2025.
+//
+
+import Testing
+@testable import GreekWordsB1
+
+struct GreekWordsB1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
