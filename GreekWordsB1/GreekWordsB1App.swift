453c1a1b0e54ac9d971c06886b0791b62687320e
Azonaz <malinina.a@gmail.com>
2025-11-03 13:27:43 +0200

    Added launch screen and appIcon

--- GreekWordsB1/GreekWordsB1App.swift
+++ GreekWordsB1/GreekWordsB1App.swift
@@ -3,10 +3,23 @@ import SwiftData
 
 @main
 struct GreekWordsB1App: App {
+    @State private var showLaunchScreen = true
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .modelContainer(for: [Word.self, GroupMeta.self, WordProgress.self, QuizStats.self])
+            if showLaunchScreen {
+                LaunchScreenView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            withAnimation {
+                                showLaunchScreen = false
+                            }
+                        }
+                    }
+            } else {
+                ContentView()
+                    .modelContainer(for: [Word.self, GroupMeta.self, WordProgress.self, QuizStats.self])
+            }
         }
     }
 }
---
+++ GreekWordsB1/Views/LaunchScreenView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct LaunchScreenView: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+    var sizeLogo: CGFloat {
+        sizeClass == .regular ? 250 : 150
+    }
+
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.05)
+                .ignoresSafeArea()
+
+            VStack {
+                Image(.launchLogo)
+                    .resizable()
+                    .frame(width: sizeLogo, height: sizeLogo)
+            }
+        }
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
